f31d4bc43a93855179fadab54a85bfe3af250691
Hosamov <hosamov@hotmail.com>
2021-11-29 16:51:59 -0800

    add data files, formatting

---
+++ js/data.js
@@ -0,0 +1,74 @@
+const data = [
+  {
+    sibling: {
+      name: "Roy",
+      rank: 1,
+    },
+  },
+  {
+    sibling: {
+      name: "Sally",
+      rank: 2,
+    },
+  },
+  {
+    sibling: {
+      name: "Jason",
+      rank: 3,
+    },
+  },
+  {
+    sibling: {
+      name: "Jonny",
+      rank: 4,
+    },
+  },
+  {
+    sibling: {
+      name: "Nathan",
+      rank: 5,
+    },
+  },
+  {
+    sibling: {
+      name: "Abby",
+      rank: 6,
+    },
+  },
+  {
+    sibling: {
+      name: "Kenny",
+      rank: 7,
+    },
+  },
+  {
+    sibling: {
+      name: "Charles",
+      rank: 8,
+    },
+  },
+  {
+    sibling: {
+      name: "Reuben",
+      rank: 9,
+    },
+  },
+  {
+    sibling: {
+      name: "Jesse",
+      rank: 10,
+    },
+  },
+  {
+    sibling: {
+      name: "Savannah",
+      rank: 11,
+    },
+  },
+  {
+    sibling: {
+      name: "Bailey",
+      rank: 12
+    }
+  }
+]
